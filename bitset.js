8d838e4b1752722925d470bad330fbc9d696479f
artur.barkhudarian <artur.barkhudarian@softconstruct.com>
2022-08-12 18:12:43 +0400

    bitset renamed

--- bitset.js
+++ bitset.js
@@ -1,15 +1,16 @@
-const N = 10 ** 3;
+const N = 10 ** 7;
 const unsorted_unique_nums = [
     ...new Set( Array( N )
         .fill( 0 )
         .map( () => Math.round( Math.random() * ( N ) ) ) ),
 ];
 
-const bitSet = (n) => Array( n ).fill( 0 );
+const arrayOfSpecifiedLength = (n) => Array( n ).fill( 0 );
 
 const wonderSort = (arr) => {
-    const bitMap = bitSet( N );
-    const result = Array( arr.length ).fill(0);
+    const bitMap = arrayOfSpecifiedLength( N );
+    const result = arrayOfSpecifiedLength( arr.length );
+
     for ( let i = 0; i < arr.length; i++ ) {
         bitMap[arr[i]] = 1;
     }
