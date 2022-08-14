c2a9c986a89909cc1f28fbdfe74d09b3b2ea4ab7
barkhudaryan-art <artur.barkhudaryan98@gmail.com>
2022-08-14 10:57:09 +0400

    commit check

--- bitset.js
+++ bitset.js
@@ -1,27 +1,31 @@
-const N = 10 ** 7;
+const N = 10 ** 9 * 2; // 2 000 000 000
+
 const unsorted_unique_nums = [
     ...new Set( Array( N )
         .fill( 0 )
         .map( () => Math.round( Math.random() * ( N ) ) ) ),
 ];
 
-const arrayOfSpecifiedLength = (n) => Array( n ).fill( 0 );
+// const arrayOfSpecifiedLength = (n) => Array( n ).fill( 0 );
+
+// const bitVector = () => {
+//     const bitSet = arrayOfSpecifiedLength( Math.ceil(N / 32) );
+// }
 
 const wonderSort = (arr) => {
-    const bitMap = arrayOfSpecifiedLength( N );
-    const result = arrayOfSpecifiedLength( arr.length );
+    // const result = arrayOfSpecifiedLength( arr.length );
 
-    for ( let i = 0; i < arr.length; i++ ) {
-        bitMap[arr[i]] = 1;
-    }
-    let x = 0;
-    for ( let i = 0; i < bitMap.length; i++ ) {
-        if ( bitMap[i] ) {
-            result[x] = i;
-            x++;
-        }
-    }
-    return result;
+    // for ( let i = 0; i < arr.length; i++ ) {
+    //     bitSet[arr[i]] = 1;
+    // }
+    // let x = 0;
+    // for ( let i = 0; i < bitSet.length; i++ ) {
+    //     if ( bitSet[i] ) {
+    //         result[x] = i;
+    //         x++;
+    //     }
+    // }
+    return arr;
 };
 
 console.log( wonderSort( unsorted_unique_nums ) );
