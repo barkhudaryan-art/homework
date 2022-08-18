1e311b4a023347a45d3cc33e4d73183b38125f81
artur.barkhudarian <artur.barkhudarian@softconstruct.com>
2022-08-18 18:56:10 +0400

    bigInt Add & Subtract done

--- day-3/matrix.js
+++ day-3/matrix.js
@@ -1,16 +1,17 @@
-const M = 10;
+const ROW = 7;
+const COL = 8;
 
-const matrix = Array(M).fill([]).map(() => Array(M).fill(0));
+const matrix = Array( ROW ).fill( [] ).map( () => Array( COL ).fill( 0 ) );
 
-for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-        matrix[i][j] = i * 10 + j;
+for ( let i = 0; i < ROW; i++ ) {
+    for ( let j = 0; j < COL; j++ ) {
+        matrix[i][j] = i * COL + j;
     }
 }
 
-const N = M * M;
-for (let i = 0; i < N; i++) {
-    const row = ~~(i / M);
-    const col = i % M;
-    console.log(matrix[row][col])
+const N = ROW * COL;
+for ( let i = 0; i < N; i++ ) {
+    const row = ~~( i / COL );
+    const col = i % COL;
+    console.log( matrix[row][col] );
 }
--- node_fs_sandbox.js
+++ node_fs_sandbox.js
@@ -9,20 +9,18 @@ const randomNumInRange = (min, max) => {
     return Math.random() * (max-min) + min;
 }
 
-const fileWriteStream = fs.createWriteStream(path.resolve(unique_nums_file_path));
-const foo = async () => {
+const writeStream = fs.createWriteStream(unique_nums_file_path);
+(async () => {
     const obj = {};
     for (let i = 0; i < 50000000; i++) {
-        const num = Math.abs(~~randomNumInRange(100000000, 200000000));
+        const num = Math.abs(~~randomNumInRange(2e8, 250000000));
         if (obj[num]) continue;
         obj[num] = true;
-        const ableToWrite = fileWriteStream.write(`${num}\n`);
+        const ableToWrite = writeStream.write(`${num}\n`);
         if (!ableToWrite) {
             await new Promise(resolve => {
-                fileWriteStream.once('drain', resolve);
+                writeStream.once('drain', resolve);
             });
         }
     }
-}
-
-foo();
+})()
