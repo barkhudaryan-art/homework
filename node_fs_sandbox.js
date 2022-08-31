47772ebb47c15ec7b6e434ebc7f83749311ff2ae
artur.barkhudarian <artur.barkhudarian@softconstruct.com>
2022-08-31 09:48:21 +0400

    bitvector sorting #7 - DONE!

--- node_fs_sandbox.js
+++ node_fs_sandbox.js
@@ -6,7 +6,6 @@ const RANGE_NUM_COUNT = 43960;
 
 const unique_nums_file_path = path.resolve( __dirname, '../sandbox/Unique_Unsorted_Numbers.txt' );
 
-
 const createRandomizedRange = (min, max, isCut = false) => {
     const set = [];
     for ( let i = min; i < max; isCut ? i += 2 : ++i ) {
@@ -16,7 +15,7 @@ const createRandomizedRange = (min, max, isCut = false) => {
     let curr;
     let top = set.length;
     while ( top-- ) {
-        curr = Math.floor(Math.random() * top + 1 );
+        curr = Math.floor( Math.random() * top + 1 );
         temp = set[curr];
         set[curr] = set[top];
         set[top] = temp;
@@ -29,11 +28,11 @@ const writeStream = fs.createWriteStream( unique_nums_file_path, { flags: 'a' } 
 ( async () => {
     // Creates an array of randomly ordered numbers,
     // each number is the minimum * 43,960 of a range
-    const ranges = createRandomizedRange(0, RANGE_SIZE, true);
+    const ranges = createRandomizedRange( 0, RANGE_SIZE, true );
     // 49,152 iterations
     for ( let i = 0; i < ranges.length; ++i ) {
         // Creates an array of randomly ordered unique numbers in a specified range
-        const range = createRandomizedRange(ranges[i] * RANGE_NUM_COUNT, ranges[i] * RANGE_NUM_COUNT + RANGE_NUM_COUNT );
+        const range = createRandomizedRange( ranges[i] * RANGE_NUM_COUNT, ranges[i] * RANGE_NUM_COUNT + RANGE_NUM_COUNT );
         // 43,960 iterations
         // 2,160,721,920 in total
         for ( let j = 0; j < range.length; ++j ) {
--- read_sort_write.js
+++ read_sort_write.js
@@ -1,75 +1,74 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require( 'fs' );
+const path = require( 'path' );
 
 const BIT_SIZE = 32;
 
 const setBit = (n, arr) => {
-    return arr[Math.floor(n / BIT_SIZE)] |= 1 << (n % BIT_SIZE)
+    return arr[Math.floor( n / BIT_SIZE )] |= 1 << ( n % BIT_SIZE );
 };
 
-const createVector = (arr) => {
-    const buffer = new ArrayBuffer( 536870912 )
-    const vector = new Uint32Array( buffer );
+const setVector = (arr, vector) => {
     for ( let i = 0; i < arr.length; ++i ) {
-        setBit( +arr[i], vector );
+        setBit( arr[i], vector );
     }
     return vector;
-}
+};
+
+const createByteArray = (byteLength) => {
+    return new Uint32Array( new ArrayBuffer( byteLength ) );
+};
 
 const wonderSort = (arr) => {
-    if (arr === undefined) return wonderSort.res;
-    const vector = createVector( arr );
-    const buffer = new ArrayBuffer( 17179869184 );
-    wonderSort.res = wonderSort.res || new Uint32Array( buffer );
-    for ( let i = 0; i < vector.length; ++i ) {
-        for ( let j = 0; j < BIT_SIZE; ++j ) {
-            if ( vector[i] & (1 << j) ) {
-                wonderSort.res[i * BIT_SIZE + j] = i * BIT_SIZE + j;
+    if ( arr === undefined ) {
+        const res = createByteArray( 8642887680 );
+        let index = 0;
+        for ( let i = 0; i < wonderSort.vector.length; ++i ) {
+            for ( let j = 0; j < BIT_SIZE; ++j ) {
+                if ( wonderSort.vector[i] & ( 1 << j ) ) {
+                    res[index] = i * BIT_SIZE + j;
+                    ++index;
+                }
             }
         }
+        return res;
     }
-    return wonderSort.res;
-}
+    wonderSort.vector = setVector( arr, wonderSort.vector || createByteArray( 536870912 ) );
+};
 
 // const unique_nums_file_path = path.resolve( __dirname, '../sandbox/remove.txt' );
-// const unique_nums_file_path = path.resolve(__dirname, "../sandbox/nums.txt")
-const unique_nums_file_path = path.resolve(__dirname, "../sandbox/Unique_Unsorted_Numbers.txt")
-const testText = path.resolve(__dirname, "../sandbox/test.txt")
+const unique_nums_file_path = path.resolve( __dirname, '../sandbox/Unique_Unsorted_Numbers.txt' );
+const testText = path.resolve( __dirname, '../sandbox/test.txt' );
 // const testText = path.resolve( __dirname, '../sandbox/Unique_Unsorted_Numbers.txt' );
 
-const rs = fs.createReadStream(unique_nums_file_path);
-const ws = fs.createWriteStream(testText);
+const rs = fs.createReadStream( unique_nums_file_path );
+const ws = fs.createWriteStream( testText );
 
-// const chunks = [];
 let buffer = '';
-rs.on('readable', () => {
+rs.on( 'readable', () => {
     let chunk;
-    while (null !== (chunk = rs.read())) {
+    while ( null !== ( chunk = rs.read() ) ) {
         const str = chunk.toString();
-        if (str[str.length - 1] !== ' ') {
+        if ( str[str.length - 1] !== ' ' ) {
             buffer += str;
         } else {
             buffer += str;
-            buffer = buffer.replace(/ $/, '');
-            console.log('sss')
-            wonderSort(buffer.split(' '))
-            // console.log('sss')
-            // console.log(wonderSort(buffer.split(' ')))
-
+            buffer = buffer.replace( / $/, '' );
+            wonderSort( buffer.split( ' ' ) );
             buffer = '';
         }
     }
-});
-rs.on('end', () => {
+} );
+rs.on( 'end', () => {
     ( async () => {
-        const ableToWrite = ws.write(`${buffer} `);
-        if (!ableToWrite) {
-            await new Promise(resolve => {
-                ws.once('drain', resolve);
-            });
+        const sortedArr = wonderSort();
+        for ( let i = 0; i < sortedArr.length; i++ ) {
+            const ableToWrite = ws.write( `${sortedArr[i]} ` );
+            if ( !ableToWrite ) {
+                await new Promise( resolve => {
+                    ws.once( 'drain', resolve );
+                } );
+            }
         }
-    })()
-    // console.log(wonderSort())
-    console.log('finished');
-});
-// rs.pipe(ws);
+    } )();
+    console.log( 'finished' );
+} );
